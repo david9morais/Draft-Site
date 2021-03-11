008496490e85cd7a6d7a6cd2a0d82bd3d8004513
david9morais <59342784+david9morais@users.noreply.github.com>
2021-03-11 00:15:21 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 import Navbar from "./components/Navbar/Navbar";
 import TaskList from "./components/TaskList/TaskList";
 
+let idAcc = 0;
+const generateId = () => {
+  idAcc = idAcc + 1;
+  return idAcc;
+};
+
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title, state) => {
+    const newTask = {
+      id: generateId(),
+      title,
+      state
+    };
+    setTasks((existingTasks) => {
+      return [...existingTasks, newTask];
+    });
+  };
+
   return (
     <div className="App">
       <Navbar />
-      <div>
-        <TaskList title="Pendent" />
-        <TaskList title="Making" />
-        <TaskList title="Finished" />
+      <div className="container">
+        <TaskList title="Pendent" onAddTask={addTask} tasks={tasks} />
       </div>
     </div>
   );
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,11 +1,29 @@
 import React from "react";
 import "./tasklist.css";
+import PropTypes from "prop-types";
 
-export default function TaskList(props) {
+export default function TaskList({ title, onAddTask, tasks }) {
+  // const [taskss, setTasks] = useState([]);
+
+  const addTask = (title, state) => {
+    onAddTask("New Task", "Pendent");
+  };
+
   return (
     <div className="tasklist">
-      <div className="title">{props.title}</div>
-      <div className="content">{}</div>
+      <div className="title">{title}</div>
+      <div className="content">
+        {tasks.map((task) => {
+          return <div key={task.id}>{task.title}</div>;
+        })}
+      </div>
+      <button onClick={addTask}>Add Task</button>
     </div>
   );
 }
+
+TaskList.propTypes = {
+  title: PropTypes.string.isRequired,
+  onAddTask: PropTypes.func.isRequired,
+  tasks: PropTypes.array.isRequired
+};
